937e2408e4727f8f35714b09e385849cd86cd73f
HarivigneshN <46593280+HarivigneshN@users.noreply.github.com>
2019-02-01 21:03:55 +0530

    Update vowel finder.py

--- vowel finder.py
+++ vowel finder.py
@@ -1,6 +1,8 @@
 vowel=["a","e","i","o","u"]
 a=input()
 if a in vowel:
-	print ("Given is vowel")
+	print ("Vowel")
+elif a.isalpha==True:
+	print("Consonant")
 else:
-	print("Given is consonant")
+	print ("Invalid")
