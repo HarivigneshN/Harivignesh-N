9c4d69c571f11430194047710597c91be2a3d4f9
HarivigneshN <46593280+HarivigneshN@users.noreply.github.com>
2019-02-01 21:11:20 +0530

    Update vowel finder.py

--- vowel finder.py
+++ vowel finder.py
@@ -2,7 +2,7 @@ vowel=["a","e","i","o","u"]
 a=input()
 if a in vowel:
 	print ("Vowel")
-elif a.isalpha==True:
+elif ((a>='a' and a<='z') or (a>='A' and a<='Z')):
 	print("Consonant")
 else:
 	print ("Invalid")
