9059894e4e6db287a679260434220572e72ce406
HarivigneshN <46593280+HarivigneshN@users.noreply.github.com>
2019-02-04 14:45:16 +0530

    Update Prime Number.py

--- Prime Number.py
+++ Prime Number.py
@@ -1,5 +1,12 @@
-a=int(input())
-if (a%2==0) or (a%3==0):
-	print ("no")
+a = int(input())
+count = 0
+
+for i in range(2, (a//2 + 1)):
+    if(a % i == 0):
+        count = count + 1
+        break
+
+if (count == 0 and a != 1):
+    print("yes")
 else:
-	print ("yes")
+    print("no")
