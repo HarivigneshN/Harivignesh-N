9dafe747568ca9c721a47199584637f674ca290f
HarivigneshN <46593280+HarivigneshN@users.noreply.github.com>
2019-06-04 09:09:27 +0530

    Update Split.py

--- Split.py
+++ Split.py
@@ -1,2 +1,9 @@
 a = [x for x in input().split()]
 print(a)
+#input---12 23---
+#output---['12','23'] this will creat a list
+a,b= input("Please enter two digits separated by space").split()
+print(a,b)
+#input---12 23---
+#output---12 23---- this will split the input
+
