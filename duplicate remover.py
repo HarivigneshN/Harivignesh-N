6dabac916537537b48d805c522f4d702a619c7d5
HarivigneshN <46593280+HarivigneshN@users.noreply.github.com>
2019-06-06 10:30:31 +0530

    Create duplicate remover.py

---
+++ duplicate remover.py
@@ -0,0 +1,6 @@
+mylist = ["a", "b", "a", "c", "c"]
+mylist = list(dict.fromkeys(mylist))
+print(mylist) 
+---------------------------------------------------------------------------
+use set()
+ie., a=set(a)  but cant sort again.
