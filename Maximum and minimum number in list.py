1a99b00145079d5707777e00b36620ca818a8cd0
HarivigneshN <46593280+HarivigneshN@users.noreply.github.com>
2019-06-07 11:34:33 +0530

    Update and rename Maximum number in list.py to Maximum and minimum number in list.py

---
+++ Maximum and minimum number in list.py
@@ -0,0 +1,20 @@
+b=int(input())
+a=input().split()
+c=min(a)
+c=int(c)
+d=abs(c)
+if c==d:
+	print(c)
+else:
+	print(max(a))
+
+-----------------------------------------------------#max below
+#b=int(input())
+#a=input().split()
+#c=max(a)
+#c=int(c)
+#d=abs(c)
+#if c==d:
+#	print(c)
+#else:
+#	print(min(a))
--- Maximum number in list.py
+++
@@ -1,9 +0,0 @@
-b=int(input())
-a=input().split()
-c=max(a)
-c=int(c)
-d=abs(c)
-if c==d:
-	print(c)
-else:
-	print(min(a))
